c08cd6470a18b7140c95790124ea731cd517779e
James Moynihan <jrmoynihan@gmail.com>
2022-12-19 08:16:32 -0500

    fix: translateY was using translateX

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,6 @@
-import Fa from './fa.svelte';
-import FaLayers from './fa-layers.svelte';
 import FaLayersText from './fa-layers-text.svelte';
+import FaLayers from './fa-layers.svelte';
+import Fa from './fa.svelte';
 
 export default Fa;
-export {
-  Fa,
-  FaLayers,
-  FaLayersText,
-};
+export { Fa, FaLayers, FaLayersText };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,126 +1,125 @@
-import type { IconStyle } from "@fortawesome/fontawesome-common-types";
 export type IconSize =
-  | "xs"
-  | "sm"
-  | "lg"
-  | "1x"
-  | "2x"
-  | "3x"
-  | "4x"
-  | "5x"
-  | "6x"
-  | "7x"
-  | "8x"
-  | "9x"
-  | "10x"
-  | `${number}x`
-  | "";
-export type PullDirection = "left" | "right" | "";
-export type FlipDirection = "horizontal" | "vertical" | "both" | false;
+	| 'xs'
+	| 'sm'
+	| 'lg'
+	| '1x'
+	| '2x'
+	| '3x'
+	| '4x'
+	| '5x'
+	| '6x'
+	| '7x'
+	| '8x'
+	| '9x'
+	| '10x'
+	| `${number}x`
+	| '';
+export type PullDirection = 'left' | 'right' | '';
+export type FlipDirection = 'horizontal' | 'vertical' | 'both' | false;
 
-export function joinCss(obj: any, separator = ";") {
-  let texts;
-  if (Array.isArray(obj)) {
-    texts = obj.filter((text) => text);
-  } else {
-    texts = [];
-    for (const prop in obj) {
-      if (obj[prop]) {
-        texts.push(`${prop}:${obj[prop]}`);
-      }
-    }
-  }
-  return texts.join(separator);
+export function joinCss(obj: any, separator = ';') {
+	let texts;
+	if (Array.isArray(obj)) {
+		texts = obj.filter((text) => text);
+	} else {
+		texts = [];
+		for (const prop in obj) {
+			if (obj[prop]) {
+				texts.push(`${prop}:${obj[prop]}`);
+			}
+		}
+	}
+	return texts.join(separator);
 }
 
 export function getStyles(
-  style: string,
-  size: IconSize,
-  pull?: PullDirection,
-  fw?: boolean
+	style: string,
+	size: IconSize,
+	pull?: PullDirection,
+	fw?: boolean
 ) {
-  let float;
-  let width;
-  const height = "1em";
-  let lineHeight;
-  let fontSize;
-  let textAlign;
-  let verticalAlign = "-.125em";
-  const overflow = "visible";
+	let float;
+	let width;
+	const height = '1em';
+	let lineHeight;
+	let fontSize;
+	let textAlign;
+	let verticalAlign = '-.125em';
+	const overflow = 'visible';
 
-  if (fw) {
-    textAlign = "center";
-    width = "1.25em";
-  }
+	if (fw) {
+		textAlign = 'center';
+		width = '1.25em';
+	}
 
-  if (pull) {
-    float = pull;
-  }
+	if (pull) {
+		float = pull;
+	}
 
-  if (size) {
-    if (size == "lg") {
-      fontSize = "1.33333em";
-      lineHeight = ".75em";
-      verticalAlign = "-.225em";
-    } else if (size == "xs") {
-      fontSize = ".75em";
-    } else if (size == "sm") {
-      fontSize = ".875em";
-    } else {
-      fontSize = size.replace("x", "em");
-    }
-  }
+	if (size) {
+		if (size == 'lg') {
+			fontSize = '1.33333em';
+			lineHeight = '.75em';
+			verticalAlign = '-.225em';
+		} else if (size == 'xs') {
+			fontSize = '.75em';
+		} else if (size == 'sm') {
+			fontSize = '.875em';
+		} else {
+			fontSize = size.replace('x', 'em');
+		}
+	}
 
-  return joinCss([
-    joinCss({
-      float,
-      width,
-      height,
-      "line-height": lineHeight,
-      "font-size": fontSize,
-      "text-align": textAlign,
-      "vertical-align": verticalAlign,
-      "transform-origin": "center",
-      overflow,
-    }),
-    style,
-  ]);
+	return joinCss([
+		joinCss({
+			float,
+			width,
+			height,
+			'line-height': lineHeight,
+			'font-size': fontSize,
+			'text-align': textAlign,
+			'vertical-align': verticalAlign,
+			'transform-origin': 'center',
+			overflow,
+		}),
+		style,
+	]);
 }
 
 export function getTransform(
-  scale: string | number,
-  translateX: string | number,
-  translateY: string | number,
-  rotate: number | string,
-  flip: FlipDirection,
-  translateTimes = 1,
-  translateUnit = "",
-  rotateUnit = ""
+	scale: string | number,
+	translateX: string | number,
+	translateY: string | number,
+	rotate: number | string,
+	flip: FlipDirection,
+	translateTimes = 1,
+	translateUnit = '',
+	rotateUnit = ''
 ): string {
-  let flipX = 1;
-  let flipY = 1;
+	let flipX = 1;
+	let flipY = 1;
 
-  if (flip) {
-    if (flip == "horizontal") {
-      flipX = -1;
-    } else if (flip == "vertical") {
-      flipY = -1;
-    } else {
-      flipX = flipY = -1;
-    }
-  }
-  const tX = parseFloat(translateX?.toString() ?? translateX);
-  const tY = parseFloat(translateX?.toString() ?? translateY);
-  const scal = parseFloat(scale?.toString() ?? scale);
+	if (flip) {
+		if (flip == 'horizontal') {
+			flipX = -1;
+		} else if (flip == 'vertical') {
+			flipY = -1;
+		} else {
+			flipX = flipY = -1;
+		}
+	}
+	const tX = parseFloat(translateX?.toString() ?? translateX);
+	const tY = parseFloat(translateY?.toString() ?? translateY);
+	const scal = parseFloat(scale?.toString() ?? scale);
 
-  return joinCss(
-    [
-      `translate(${tX * translateTimes}${translateUnit},${
-        tY * translateTimes
-      }${translateUnit})`,
-      `scale(${flipX * scal},${flipY * scal})`,
-      rotate && `rotate(${rotate}${rotateUnit})`,
-    ],
-    " "
-  );
+	return joinCss(
+		[
+			`translate(${tX * translateTimes}${translateUnit},${
+				tY * translateTimes
+			}${translateUnit})`,
+			`scale(${flipX * scal},${flipY * scal})`,
+			rotate && `rotate(${rotate}${rotateUnit})`,
+		],
+		' '
+	);
 }
